ca2824e5afe20647cf1a2d2521203047fccc32bf
Marc von Holzen <marc@vonholzen.org>
2024-02-04 22:18:04 -0800

    add headers to table

--- transforms/html/table.go
+++ transforms/html/table.go
@@ -9,7 +9,7 @@ type html string
 
 func FromTriples(data *triples.Triples) (html, error) {
 	tpls := data.GetTriplesForPredicate(triples.NewStringNode("html")).GetTripleList()
-	if len(tpls) >= 0 {
+	if len(tpls) > 0 {
 		res := ""
 		for _, tpl := range tpls {
 			res += tpl.Object.String()
@@ -23,7 +23,7 @@ func FromTriples(data *triples.Triples) (html, error) {
 func HtmlTableMapper(source *triples.Triples) (*triples.Triples, error) {
 	res := triples.NewTriples()
 	container := triples.NewAnonymousNode()
-	htmlTable := table((source))
+	htmlTable := table(source)
 	res.AddTripleFromAny(container, "html", triples.NewStringNode(htmlTable))
 	return res, nil
 }
@@ -33,30 +33,46 @@ func table(source *triples.Triples) string {
 	if err != nil {
 		return ""
 	}
+	// exclude rows triples from the data
+	// to avoid the predice list containing row indices
+	data := source.Clone()
+	data.DeleteTriples(rows)
+
+	headers := data.GetPredicateList().SortLexical()
+
 	html := "<table>\n"
+	html += headerrow(headers)
 	for _, triple := range rows.GetTripleList().Sort() {
-		html += tablerow(triple, source)
+		html += tablerow(triple, headers, data)
 	}
 	html += "\n</table>\n"
 	return html
 }
 
-func tablerow(triple triples.Triple, source *triples.Triples) string {
-	rows := source.GetTriplesForSubject(triple.Object).GetTripleList().Sort()
-	if len(rows) == 0 {
-		return ""
-	}
+func headerrow(headers triples.NodeList) string {
 	res := "<tr>\n"
-	for _, row := range rows {
-		res += tablecell(row.Object, source)
+	for _, node := range headers {
+		res += "<th>" + node.String() + "</th>\n"
 	}
 	res += "</tr>\n"
 	return res
 }
 
-func tablecell(node triples.Node, source *triples.Triples) string {
-	res := "<td>\n"
-	for _, cell := range source.GetTriplesForSubject(node).GetTripleList().Sort() {
+func tablerow(triple triples.Triple, headers triples.NodeList, source *triples.Triples) string {
+	res := "<tr>\n"
+
+	for _, header := range headers {
+		cellTriples := source.GetTriplesForSubjectPredicate(triple.Object, header)
+		res += tablecell(cellTriples, source)
+	}
+
+	res += "</tr>\n"
+	return res
+}
+
+func tablecell(cellTriples, source *triples.Triples) string {
+	res := "<td>"
+	for _, cell := range cellTriples.GetTripleList().Sort() {
 		res += cell.Object.String() + "<br>\n"
 	}
 	res += "</td>\n"
